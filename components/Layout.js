69ae3a009eaea1cc4a97a2daf9ddef388d964aa6
shinarasheed <rasheedshinaopeyemi@gmail.com>
2021-08-27 22:11:55 +0100

    refactor components

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,14 @@
 import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Products | Lumin Skincare</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Navbar />
       <section>{children}</section>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Head from "next/head";
 import Products from "../components/Products";
 import ProductsCta from "../components/ProductsCta";
 
 function Home() {
   return (
     <>
-      <Head>
-        <title>Products | Lumin Skincare</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.png" />
-      </Head>
       <ProductsCta />
       <Products />
     </>
